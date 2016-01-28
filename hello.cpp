33b9f5fb303a1a81f96430158765e876737c6f31
Tristan Wells <darkfield0@gmail.com>
2016-01-28 16:38:55 -0500

    hello test

---
+++ hello.cpp
@@ -0,0 +1,12 @@
+    1  g++ -o payroll payroll.cpp
+    2  echo "#csci155">> README.md
+    3  git init
+    4  git add README.md
+    5  git commit -m "first commit"
+    6  git remote add origin https://github.com/DarkField0/CSCI155.git
+    7  git push -u origin master
+    8  ls
+    9  clear
+   10  g++ -o payroll payroll.cpp
+   11  clear
+   12  history > hello.cpp
