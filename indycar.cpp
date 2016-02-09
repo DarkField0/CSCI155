411283d53c94cc6083867b6e1ae54cc70f1ab39f
Tristan Wells <darkfield0@gmail.com>
2016-02-09 16:12:09 -0500

    IndyCar Version 1

---
+++ indycar.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int time, speed;
+  cout << "How many seconds did the lap take?" ;
+  cin >> time;
+  speed = (2.5 / time * 3600);
+  cout << "The average speed was " << speed << "mph" << endl;
+  
+return 0;
+}
